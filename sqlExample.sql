4ce95be92a4d68ee486c6b2e719459f833c1a90a
MarkSignus <markosh94@hotmail.com>
2018-09-07 10:39:25 +0100

    sql examples

---
+++ sqlExample.sql
@@ -0,0 +1,16 @@
+!-- NEW TABLE
+!-- Explain what the table is for etc.
+
+
+!-- BUILD TABLE
+!-- Script that will delete the table, and then craete it from scratch
+
+
+!-- FILL TABLE
+!--Scipt that fills table with out dummy data
+
+
+
+
+
+!-- START HERE
